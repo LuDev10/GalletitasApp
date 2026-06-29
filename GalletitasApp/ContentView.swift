5d4984225d83b70db063d881774f2a6eb99f5b8b
Luis <diazldev@gmail.com>
2026-06-29 15:06:36 -0300

    Initial Commit

---
+++ GalletitasApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GalletitasApp
+//
+//  Created by Luis Díaz on 29/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GalletitasApp/GalletitasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalletitasAppApp.swift
+//  GalletitasApp
+//
+//  Created by Luis Díaz on 29/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GalletitasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
